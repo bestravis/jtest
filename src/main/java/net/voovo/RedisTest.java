2548418c5c88fde45f34ab3c474c74ffdf06ed14
travis2194 <travis2194@outlook.com>
2019-01-18 17:30:40 +0800

    update

--- src/main/java/net/voovo/RedisTest.java
+++ src/main/java/net/voovo/RedisTest.java
@@ -1,5 +1,12 @@
 package net.voovo;
 
+import net.voovo.redis.RedisHandle;
+
 public class RedisTest {
 
+	RedisHandle redisCli=new RedisHandle();
+	
+	public static void main(String[] args) {
+		
+	}
 }
