f238a3df6996591f4bbc389e48d28f6fe5510456
travis2194@outlook.com <travis2194@outlook.com>
2020-01-10 11:59:22 +0800

    更新

---
+++ src/main/java/net/voovo/math/chufa.java
@@ -0,0 +1,13 @@
+package net.voovo.math;
+
+/**
+ * @Author: yml
+ * @Date: 2020/1/9 10:43
+ */
+public class chufa {
+
+    public static void main(String[] args) {
+        double a=75+81+54+57;
+        System.out.println((int)Math.ceil(a/4));
+    }
+}
