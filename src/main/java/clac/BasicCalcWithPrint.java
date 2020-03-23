251ff07091ef22237a39300a3dd924eff8a8b939
travis2194@outlook.com <travis2194@outlook.com>
2020-03-23 18:00:32 +0800

    1

--- src/main/java/clac/BasicCalcWithPrint.java
+++ src/main/java/clac/BasicCalcWithPrint.java
@@ -15,7 +15,15 @@ public class BasicCalcWithPrint {
         //二进制导致浮点数失真 TODO
         System.out.println(12-11.9==0.1);
 
-        System.out.println(0.9-0.3);
+        /***
+         * 解决方法
+         * 1.用Decimal
+         */
+        double price;
+        for (int i = 1; i <= 10; i++) {
+            price=i/1000.0;
+            System.out.println(0.2+0.4);
+        }
     }
 
     // + - * /
