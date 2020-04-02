0303d7629a69e4ec475dc3aebf89356bde32fe98
travis2194@outlook.com <travis2194@outlook.com>
2020-04-02 09:05:22 +0800

    1

--- src/main/java/algorithm/quick_sort/Test.java
+++ src/main/java/algorithm/quick_sort/Test.java
@@ -27,7 +27,7 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        int arr[] = new int[]{3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321};
+        int[] arr = new int[]{3,3,3,7,9,122344,4656,34,34,4656,5,6,7,8,9,343,57765,23,12321};
         int len = arr.length-1;
         arr=qsort(arr,0,len);
         for (int i:arr) {
--- src/main/java/net/voovo/Test.java
+++ src/main/java/net/voovo/Test.java
@@ -1,10 +1,12 @@
 package net.voovo;
 
+
 public class Test {
 
 	public static void main(String[] args) {
 		double x=2.0; int y=4; x/=++y;
-		System.out.println(x);
+		System.out.println(new Integer(1));
+
 	}
 
 
