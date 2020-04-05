ff8739fb13f0239cd5cac851c03df3fda2273c7b
traivs <traivs2194@outlook.com>
2020-04-06 00:10:20 +0800

    1

--- src/main/java/algorithm/sort/bubble/Test.java
+++ src/main/java/algorithm/sort/bubble/Test.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class Test {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{10, 14, 5, 5, 6, 8};
+        int[] arr = new int[]{10, 14, 5, 5, 6, 8,123,1231,2,3};
         int pot = 0;
-        //冒泡
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i; j++) {
+        //冒泡 O(n^2)
+        for (int i = 1; i < arr.length; i++) {//控制比较次数 O(n)
+            for (int j = 0; j < arr.length - i; j++) {//比较数据 O(n))
                 if (arr[j] > arr[j + 1]) {
                     pot = arr[j];
                     arr[j] = arr[j + 1];
